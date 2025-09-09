2b26eee52c35bdd681f5a04b1b304b9559071737
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-09-09 17:24:02 -0300

    implementação de struct Endereço dentro da struct Pessoa

--- struct/struct_in_struct.c
+++ struct/struct_in_struct.c
@@ -4,12 +4,23 @@
 
 // criando novos tipos de dados:
 
-typedef struct {
-    int dia, mes, ano;
-}DataNas;
+typedef struct
+{
+    char rua [100];
+    char quadra [50];
+    char lote [50];
+    char setor [50];
+    char cidade [50];
+} Endereco;
 
 typedef struct
 {
+    int dia, mes, ano;
+} DataNas;
+
+typedef struct
+{
+    Endereco endereco;
     DataNas dataNas;
     char nome[100];
     char sexo;
@@ -24,13 +35,30 @@ int main()
     fgets(pessoa.nome, 100, stdin);
     printf("Digite a sua idade: ");
     scanf("%d", &pessoa.idade);
-    scanf("%c");
+    fgetchar();
     printf("Digite o seu sexo 'F' para faminino e 'M' para masculino: ");
     scanf("%c", &pessoa.sexo);
-    printf("Digite sua data de nascimento (sem as barras) no formato DD/MM/AAAA: ");
-    scanf("%d%d%d", &pessoa.dataNas.dia, &pessoa.dataNas.mes, &pessoa.dataNas.ano);
+    fgetchar();
+    printf("Digite sua data de nascimento no formato DD/MM/AAAA: ");
+    scanf("%02d/%02d/%04d", &pessoa.dataNas.dia, &pessoa.dataNas.mes, &pessoa.dataNas.ano);
+    fgetchar();
+    printf("Digite o nome da sua rua: ");
+    fgets(pessoa.endereco.rua, 100, stdin);
+    printf("Digite a quadra: ");
+    fgets(pessoa.endereco.quadra, 50, stdin);
+    printf("Digite o lote: ");
+    fgets(pessoa.endereco.lote, 50, stdin);
+    printf("Digite o nome do setor: ");
+    fgets(pessoa.endereco.setor, 50, stdin);
+    printf("Digite o nome da cidade: ");
+    fgets(pessoa.endereco.cidade, 50, stdin);
 
+    printf("--------------------------------------\n");
+    printf("Informacoes do usuario:\n");
+    printf("\n");
     printf("Nome: %s\nIdade: %d\nSexo: %c\nData de nas.: %d/%d/%d\n", pessoa.nome, pessoa.idade, pessoa.sexo, pessoa.dataNas.dia, pessoa.dataNas.mes, pessoa.dataNas.ano);
+    printf("\n");
+    printf("Endereco:\nRua: %sQuadra: %sLote: %sSetor: %sCidade: %s", pessoa.endereco.rua, pessoa.endereco.quadra, pessoa.endereco.lote, pessoa.endereco.setor, pessoa.endereco.cidade);
 
     return 0;
 }
